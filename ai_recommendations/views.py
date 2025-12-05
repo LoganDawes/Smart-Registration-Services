38f88df47f9ff5e9beb3581b8173ca190f6d72eb
JbrownOSU2003 <justice.brown@okstate.edu>
2025-12-05 02:46:50 -0600

    Import additional Django modules for views

--- ai_recommendations/views.py
+++ ai_recommendations/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse, HttpResponseBadRequest
 
-# Create your views here.
+# from .models import CourseRecommendation
+# Use these if you need login/permission checks:
+# from django.contrib.auth.decorators import login_required
