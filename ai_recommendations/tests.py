8c6f1eda4f6683dc1a6243859de0aeb834039dc5
JbrownOSU2003 <justice.brown@okstate.edu>
2025-12-05 02:48:37 -0600

    Import additional modules for testing

--- ai_recommendations/tests.py
+++ ai_recommendations/tests.py
@@ -1,3 +1,7 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.utils import timezone
 
-# Create your tests here.
+from .models import CourseRecommendation, RecommendationFeedback
+from authentication.models import User
+from courses.models import Course
